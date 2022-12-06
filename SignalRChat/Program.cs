e3daf1fc01d9aa5f3f4b6ac062059086a2110a32
Pavle <p.devidze@gmail.com>
2022-12-06 18:48:07 +0400

    add server SignalR

--- SignalRChat/Program.cs
+++ SignalRChat/Program.cs
@@ -1,5 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
+
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
