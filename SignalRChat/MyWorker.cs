6690d09d4d03256e6e9d1e27fe4bc3c39dac35dc
Pavle <p.devidze@gmail.com>
2022-12-07 17:41:27 +0400

    connect background service to SignalR

--- SignalRChat/MyWorker.cs
+++ SignalRChat/MyWorker.cs
@@ -1,4 +1,6 @@
-﻿public interface IWorker
+﻿using Microsoft.AspNetCore.SignalR;
+
+public interface IWorker
 {
     Task DoWork(CancellationToken cancellationToken);
 }
@@ -6,11 +8,13 @@
 public class MyWorker : IWorker
 {
     private readonly ILogger<MyWorker> logger;
+    private IHubContext<ChatHub> HubContext { get; set; }
     private int number = 0;
 
-    public MyWorker(ILogger<MyWorker> logger)
+    public MyWorker(ILogger<MyWorker> logger, IHubContext<ChatHub> hubcontext)
     {
         this.logger = logger;
+        this.HubContext = hubcontext;
     }
 
     public async Task DoWork(CancellationToken cancellationToken)
@@ -19,6 +23,7 @@ public class MyWorker : IWorker
         {
             Interlocked.Increment(ref number);
             logger.LogInformation($"Worker printing number: {number}");
+            await this.HubContext.Clients.All.SendAsync("WorkerPrinted", number);
             await Task.Delay(1000 * 5);
         }
     }
